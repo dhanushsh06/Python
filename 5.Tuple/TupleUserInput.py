093393a14b265fd26e82abe853bafc0193563904
Dhanush S H <dhanushsh700@gmail.com>
2026-08-11 14:14:15 +0530

    Add Tuple User input

---
+++ 5.Tuple/TupleUserInput.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+numbers = (a, b, c)
+
+print("Tuple:", numbers)
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+print("Sum:", sum(numbers))
