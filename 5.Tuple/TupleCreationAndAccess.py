d22d4a28949824a3045908936e0ca5781814c378
Dhanush S H <dhanushsh700@gmail.com>
2026-08-11 14:00:30 +0530

    Add Tuple Creation and Access

---
+++ 5.Tuple/TupleCreationAndAccess.py
@@ -0,0 +1,9 @@
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+print("First element:", numbers[0])
+print("Second element:", numbers[1])
+print("Third element:", numbers[2])
+print("Fourth element:", numbers[3])
+print("Last element:", numbers[-1])
+print("Length:", len(numbers))
