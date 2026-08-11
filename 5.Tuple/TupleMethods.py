d9ce53262051b5ecc5e8501902491f18847ecd36
Dhanush S H <dhanushsh700@gmail.com>
2026-08-11 14:03:10 +0530

    Add Tuple Methods

---
+++ 5.Tuple/TupleMethods.py
@@ -0,0 +1,6 @@
+numbers = (10, 20, 30, 20, 40, 20)
+
+print("Tuple:", numbers)
+print("Count of 20:", numbers.count(20))
+print("Index of 20:", numbers.index(20))
+print("Index of 30:", numbers.index(30))
