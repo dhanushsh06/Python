c7c2016e54ddbec91711d0bf9cb1fd27fa0d6963
Dhanush S H <dhanushsh700@gmail.com>
2026-08-11 14:12:31 +0530

    Add Tuple Operations

---
+++ 5.Tuple/TupleOperations.py
@@ -0,0 +1,7 @@
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+print("Concatenation:", tuple1 + tuple2)
+print("Repetition:", tuple1 * 2)
+print("Is 2 present?", 2 in tuple1)
+print("Slicing:", tuple2[0:2])
