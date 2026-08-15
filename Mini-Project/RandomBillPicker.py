6150f66ab91546aa096904297ab0baca9760e4ee
Dhanush S H <dhanushsh700@gmail.com>
2026-08-15 20:03:15 +0530

    Organize

--- Mini-Project/RandomBillPicker.py
+++
@@ -1,7 +0,0 @@
-import random
-
-Names = input("Enter the Names Separated by comma: ")
-names_list = Names.split(",")
-random_name = random.choice(names_list)
-
-print(f"{random_name} will pay the Bill")
