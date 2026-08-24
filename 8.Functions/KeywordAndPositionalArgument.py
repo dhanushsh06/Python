17bb8cc207eaf5a4f09fce7b6539cc0d5ed986d1
Dhanush S H <dhanushsh700@gmail.com>
2026-08-24 16:03:54 +0530

    Add Keyword and Positional arguments

---
+++ 8.Functions/KeywordAndPositionalArgument.py
@@ -0,0 +1,6 @@
+def student(name,age):
+    print(f"Name: {name}\n" f"Age: {age}")
+def student2(name2,age2):
+    print(f"Name 2: {name2}\n"f"Age 2: {age2}")
+student(name = "dhan",age = 20)
+student2("xyz",21)              
