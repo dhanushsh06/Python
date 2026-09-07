e972f6856773067b18f7584b14153a94e62ea96f
Dhanush S H <dhanushsh700@gmail.com>
2026-09-07 23:40:50 +0530

    Add User input and Methods of Dictionary

---
+++ 9.Dictionary/UserInputAndMethods.py
@@ -0,0 +1,10 @@
+student = {}
+student["Name"] = "XYZ"
+student["Age"] = 21
+student["Course"] = "CS"
+
+print(f"After user input:{student}")
+
+print(f"Key:{student.keys()}")
+print(f"Value:{student.values()}")
+print(f"Items:{student.items()}")
